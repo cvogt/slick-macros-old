80ed12e3272c5da36e658741d2adb1153b95ef9a
Christopher Vogt <github.com.nsp@cvogt.org>
2012-03-29 00:29:29 +0200

    - obscure bugs with typechecking and reification in macros

--- src/main/scala/slick/Queryable.scala
+++ src/main/scala/slick/Queryable.scala
@@ -5,10 +5,12 @@ final case class column(name:String) extends StaticAnnotation
 import scala.reflect.makro.Context
 
 object Queryable{
-  def apply[T](q:Queryable[T]) = new Queryable[T](q.query) // TODO: make this a macro
-  def apply[T:Manifest] = new Queryable[T](scala2scalaquery.classToQuery[T])
-  def factory[S]( projection:scala.reflect.mirror.Expr[Queryable[S]] ) : Queryable[S] = {
-    new Queryable[S](scala2scalaquery( projection.tree ))
+  def apply[T:Manifest] = new Queryable[T]
+  def factory[S](driver:Any, projection:Any ) : Queryable[S] = {
+    println("factory")
+    val q = new Queryable[S]
+    println("factory done")
+    q
   }
 }
 
@@ -23,13 +25,13 @@ case class Utils[C <: Context]( c:C ) {
       super.transform {
         tree match {
           case  //Apply( // needed to account for ApplyToImplicitArgs
-                Apply(TypeApply(Select(_this, termname), _), reified::Nil )
+                Apply(TypeApply(Select(_this, termname), _), List( _ , reified ) )
                 //,_)
                 if termname.toString == "factory" => c.unreifyTree(reified)
           case //Apply(
-              Apply(Select(_this, termname), reified::Nil )
+              Apply(Select(_this, termname), List( _ , reified ) )
               //,_)
-              if termname.toString == "factory" => c.unreifyTree(reified) 
+              if termname.toString == "factory" => c.unreifyTree(reified)
           case _ => tree
         }
       }
@@ -38,38 +40,39 @@ case class Utils[C <: Context]( c:C ) {
 }
 
 object QueryableMacros{
-  private def _helper[C <: Context,S:c.TypeTag]( c:C )( name:String, projection:c.mirror.Expr[_] ) = {
+  private def _helper[C <: Context{type PrefixType = Queryable[T]},T:c.TypeTag,S:c.TypeTag]( c:C )( name:String, projection:c.mirror.Expr[_] ) = {
     import c.mirror._
     val element_type = implicitly[TypeTag[S]].tpe
-    // 
+
+    //val qname = newTermName( "q123412341" ) //FIXME: c.fresh )
+    //val qdef = Expr[Unit]( ValDef(NoMods, qname, TypeTree().setType(c.prefix.tree.tpe), c.prefix.tree) )
+    //val quse = Expr[ Queryable[T] ]( Ident(qname) )
+/*    println(showRaw(        Utils[c.type](c).removeDoubleReify(
+          Apply(Select(c.prefix.tree, newTermName( "_"+name+"_placeholder" )), List( projection.tree ))
+         ).asInstanceOf[Tree]
+        ))
+*/
     val reifiedExpression = Expr[reflect.mirror.Expr[Queryable[S]]](
-    c.reifyTree( c.reflectMirrorPrefix, c.typeCheck(
-      Utils[c.type](c).removeDoubleReify(
-        Apply(Select(c.prefix.tree, newTermName( "_"+name+"_placeholder" )), List( projection.tree ))
-       ).asInstanceOf[Tree]
-      )))
-    c.reify{ Queryable.factory[S]( reifiedExpression.eval )}
+      c.reifyTree( c.reflectMirrorPrefix, c.typeCheck(
+        Utils[c.type](c).removeDoubleReify(
+          Apply(Select(c.prefix.tree, newTermName( "_"+name+"_placeholder" )), List( projection.tree ))
+         ).asInstanceOf[Tree]
+        )
+      )
+    )
+    c.reify{
+      Queryable.factory[S](c.prefix.eval.driver, null ) // -> TestQueryable.scala:4: error: exception during macro expansion: scala.reflect.internal.Types$TypeError: value _filter_placeholder is not a member of type parameter T
+      //Queryable.factory[S](c.prefix.eval.driver, reifiedExpression.eval ) // -> java.lang.ClassCastException: scala.reflect.api.Trees$ValDef cannot be cast to scala.collection.Seq
+    }
   }
 
-  def map[T:c.TypeTag, S:c.TypeTag]
-               (c: scala.reflect.makro.Context)
-               (projection: c.mirror.Expr[T => S]): c.mirror.Expr[scala.slick.Queryable[S]] = _helper[c.type,S]( c )( "map", projection )
-  def flatMap[T:c.TypeTag, S:c.TypeTag]
-               (c: scala.reflect.makro.Context)
-               (projection: c.mirror.Expr[T => Queryable[S]]): c.mirror.Expr[scala.slick.Queryable[S]] = _helper[c.type,S]( c )( "flatMap", projection )
   def filter[T:c.TypeTag]
-               (c: scala.reflect.makro.Context)
-               (projection: c.mirror.Expr[T => Boolean]): c.mirror.Expr[scala.slick.Queryable[T]] = _helper[c.type,T]( c )( "filter", projection )
+               (c: scala.reflect.makro.Context{type PrefixType = Queryable[T]})
+               (projection: c.mirror.Expr[T => Boolean]): c.mirror.Expr[scala.slick.Queryable[T]] = _helper[c.type,T,T]( c )( "filter", projection )
 }
 
-class Queryable[T]( protected[slick] val query:scala2scalaquery.Query ){
-  def dump = scala2scalaquery.dump(query)
-  def _map_placeholder[S]( projection: T => S ) : Queryable[S] = ???
-  def map[S]( projection: T => S ) : Queryable[S] = macro QueryableMacros.map[T,S]
-  
-  def _flatMap_placeholder[S]( projection: T => Queryable[S] ) : Queryable[S] = ???
-  def flatMap[S]( projection: T => Queryable[S] ) : Queryable[S] = macro QueryableMacros.flatMap[T,S]
-
+class Queryable[T]{
+  val driver = null
   def _filter_placeholder( projection: T => Boolean ) : Queryable[T] = ???
   def filter( projection: T => Boolean ) : Queryable[T] = macro QueryableMacros.filter[T]
 }
